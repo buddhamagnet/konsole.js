0837344eb2b8a1d75dad57fa86a8326ed795bf91
buddhamagnet <buddhamagnet@gmail.com>
2013-07-18 12:30:27 +0000

    add rest of console api

--- ec_console.js
+++ ec_console.js
@@ -2,7 +2,13 @@ if (Econ.debug()) {
 
   Econ.console = Econ.console || {
 
-    console_api: ['log', 'dir', 'trace'],
+    consoleApi: ['assert', 'clear', 'count', 'debug', 'dir', 'dirxml', 'error', 'exception', 'info', 'log', 'table', 'trace', 'warn'],
+
+    timerApi: ['time', 'timeStamp', 'timeEnd'],
+    
+    profilerApi: ['profile', 'profileEnd'],
+    
+    groupApi: ['group', 'groupCollapsed', 'groupEnd'],
 
     register_function: function(fn) {
       if (typeof console === "undefined") {
@@ -12,17 +18,24 @@ if (Econ.debug()) {
         console[fn] = function() {};
       }
       if (typeof console !== "undefined" && typeof console[fn] === "function") {
+        console.log('adding ' + fn);
         this[fn] = function() {
           return console[fn].apply(console, arguments);
         };
       }
     },
 
-    init: function() {
-      var i, len = this.console_api.length;
+    register_api: function(api) {
+      var i, len = this[api].length;
       for (i = 0; i < len; i++) {
-        this.register_function(this.console_api[i]);
+        this.register_function(this[api][i]);
       }
+      return this;
+
+    },
+
+    init: function() {
+      this.register_api('consoleApi').register_api('timerApi');
     }
 
   };
